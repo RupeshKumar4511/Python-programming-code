68449ef8fb14e349c73605a5b1f627c040c2d8b7
Rupesh Kumar <kumarrn4511@gmail.com>
2024-03-28 13:59:30 +0530

    this is a python program

---
+++ first.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([1,23])
+y = np.array([5,28])
+plt.plot(x,y)
+plt.title("line plot")
+plt.xlabel("X axis")
+plt.ylabel("y axis")
+plt.show()
